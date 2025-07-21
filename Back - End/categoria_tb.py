bb99ab7567edcb5b71fa6f2aae28e18b7f679d59
gay <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-07-21 14:43:12 -0300

    Create categoria_tb.py

---
+++ Back - End/categoria_tb.py
@@ -0,0 +1,8 @@
+nome = input("Digite o nome da categoria: ")
+descricao = input("Digite a descrição da categoria: ")
+status = input("Digite o status da categoria (Ativa/Inativa): ")
+
+print("\n=== CATEGORIA CADASTRADA ===")
+print("Nome:", nome)
+print("Descrição:", descricao)
+print("Status:", status)

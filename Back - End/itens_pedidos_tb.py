1b3806e0450aa7bffee98eddbce4e495f2baf662
gay <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-07-21 15:08:57 -0300

    Create itens_pedidos_tb.py

---
+++ Back - End/itens_pedidos_tb.py
@@ -0,0 +1,6 @@
+quantidade = input("Digite a quantidade do item: ")
+preco_total = input("Digite o preço total do item (ex: 50.00): ")
+
+print("\n=== ITEM DO PEDIDO ===")
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)

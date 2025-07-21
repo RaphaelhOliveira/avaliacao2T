0f0ef5c5b83dadc9ba2aab3cd94470829c63578c
gay <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-07-21 14:39:02 -0300

    Create feedback_tb.py

---
+++ Back - End/feedback_tb.py
@@ -0,0 +1,8 @@
+nota = input("Digite uma nota de 0 a 10: ")
+comentario = input("Deixe um comentário: ")
+data = input("Digite a data (ex: 21/07/2025): ")
+
+print("\n=== FEEDBACK REGISTRADO ===")
+print("Nota:", nota)
+print("Comentário:", comentario)
+print("Data:", data)

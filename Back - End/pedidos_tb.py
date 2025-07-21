dead2bd9aca2578616fc68b062a72bb794c234f5
gay <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-07-21 14:57:47 -0300

    Create pedidos_tb.py

---
+++ Back - End/pedidos_tb.py
@@ -0,0 +1,6 @@
+data = input("Digite a data do pedido (ex: 21/07/2025): ")
+status = input("Digite o status do pedido (ex: Pendente, Enviado, Entregue): ")
+
+print("\n=== DADOS DO PEDIDO ===")
+print("Data do pedido:", data)
+print("Status:", status)

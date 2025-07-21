2c3841eafe9de8b4a05e723e675a00731cd9a27a
gay <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-07-21 14:55:01 -0300

    Create estoque_tb.py

---
+++ Back - End/estoque_tb.py
@@ -0,0 +1,8 @@
+quantidade = input("Digite a quantidade em estoque: ")
+validade = input("Digite a data de validade (ex: 30/12/2025): ")
+ultima_atualizacao = input("Digite a data da última atualização (ex: 21/07/2025): ")
+
+print("\n=== DADOS DO ESTOQUE ===")
+print("Quantidade:", quantidade)
+print("Validade:", validade)
+print("Última atualização:", ultima_atualizacao)

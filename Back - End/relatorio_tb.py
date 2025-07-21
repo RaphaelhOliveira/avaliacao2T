25c5b53ead0d7dfbf652debf75fdebd8aadb9267
gay <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-07-21 15:05:44 -0300

    Create relatorio_tb.py

---
+++ Back - End/relatorio_tb.py
@@ -0,0 +1,8 @@
+tipo = input("Digite o tipo do relatório: ")
+pedido_inicial = input("Digite o número do pedido inicial: ")
+pedido_final = input("Digite o número do pedido final: ")
+
+print("\n=== RELATÓRIO GERADO ===")
+print("Tipo:", tipo)
+print("Pedido inicial:", pedido_inicial)
+print("Pedido final:", pedido_final)

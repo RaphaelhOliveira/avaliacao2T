84371a5e94b68b3badb757016323c8c8ac9183ac
gay <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-07-21 15:01:52 -0300

    Create rastreamento_tb.py

---
+++ Back - End/rastreamento_tb.py
@@ -0,0 +1,8 @@
+latitude = input("Digite a latitude: ")
+longitude = input("Digite a longitude: ")
+data = input("Digite a data do rastreamento (ex: 21/07/2025): ")
+
+print("\n=== DADOS DE RASTREAMENTO ===")
+print("Latitude:", latitude)
+print("Longitude:", longitude)
+print("Data:", data)
